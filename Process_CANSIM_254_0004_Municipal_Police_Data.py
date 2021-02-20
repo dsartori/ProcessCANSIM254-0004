1ff3a9e50d8368b0d6ef04e2cd5d3741b0c89b73
Douglas Sartori <doug@sartori.ca>
2021-02-20 14:09:26 -0500

    initial commit

--- Process_CANSIM_254_0004_Municipal_Police_Data.py
+++ Process_CANSIM_254_0004_Municipal_Police_Data.py
@@ -1,1 +1,21 @@
 
+import os
+import petl
+import sys
+import logging as log
+from datetime import date
+
+
+log.basicConfig(level=os.environ.get("LOGLEVEL","INFO"))
+
+inputFile='./35100077.csv'
+try:
+    cansimData = petl.fromcsv(inputFile)
+
+except Exception as e:
+    log.error(e)
+
+cansimData = petl.cut(cansimData,0,'GEO','DGUID','Statistics','VALUE')
+
+outputData = petl.recast(cansimData,variablefield='Statistics',valuefield='VALUE')
+petl.io.csv.tocsv(outputData,'output.csv')
